02a594ab1e80bb1a4e0c278183df946bca6487a5
Kate Sky <kateskytech@gmail.com>
2017-11-02 17:18:24 -0500

    commit 1

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+
+// Register Service Worker
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    // Registration was successful
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+// Setup Push notifications
+
+var pushButton = document.querySelector('.js-push-button');
+pushButton.addEventListener('click', subscribe);
+
+function subscribe() {
+  // Disable the button so it can't be changed while  
+  // we process the permission request  
+  pushButton.disabled = true;
+
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    serviceWorkerRegistration.pushManager.subscribe({ userVisibleOnly: true })
+      .then(function(subscription) {
+        // The subscription was successful  
+        isPushEnabled = true;
+        pushButton.textContent = 'Disable Push Messages';
+        pushButton.disabled = false;
+
+        // TODO: Send the subscription.endpoint to your server  
+        // and save it to send a push message at a later date
+        // return sendSubscriptionToServer(subscription);
+      })
+      .catch(function(e) {
+        if(Notification.permission === 'denied') {
+          // The user denied the notification permission which  
+          // means we failed to subscribe and the user will need  
+          // to manually change the notification permission to  
+          // subscribe to push messages  
+          console.warn('Permission for Notifications was denied');
+          pushButton.disabled = true;
+        } else {
+          // A problem occurred with the subscription; common reasons  
+          // include network errors, and lacking gcm_sender_id and/or  
+          // gcm_user_visible_only in the manifest.  
+          console.error('Unable to subscribe to push.', e);
+          pushButton.disabled = false;
+          pushButton.textContent = 'Enable Push Messages';
+        }
+      });
+  });
+}
