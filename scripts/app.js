5474c53618f894194d1069975760d5bee582b58d
Kate Sky <kateskytech@gmail.com>
2017-11-08 15:24:33 -0600

    added notifications

--- scripts/app.js
+++ scripts/app.js
@@ -10,11 +10,19 @@
     methods: {
         addtoCart: function (item) {
             this.items.push(item)
+            localStorage.setItem('cart', JSON.stringify(this.items));
         },
         
 
     },
-    mounted:function() {
+    mounted: function () {
+        var items = localStorage.getItem('cart');
+        if (items) {
+            this.items = JSON.parse(items);
+            notifyMe("you have cart items")
+
+        }
+
         var url = "/pwa/data/data.json"
         var promise = Promise.resolve($.getJSON(url))
         promise.then(function (response, statusText, xhrObj) {
@@ -28,3 +36,30 @@
     }
 })
 
+
+
+function notifyMe(msg) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification(msg);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification(msg);
+            }
+        });
+    }
+
+    // At last, if the user has denied notifications, and you 
+    // want to be respectful there is no need to bother them any more.
+}
